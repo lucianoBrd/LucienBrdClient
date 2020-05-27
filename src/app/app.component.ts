c5f8f41eea36af49a41b7a05dd9c52dc48e3da07
lucianoBrd <lucien.burdet@gmail.com>
2020-05-27 11:41:50 +0200

    dark/light + rtl/ltr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CustomizerService } from './shared/service/customizer.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public url: any;
   title = 'lucienBrdClient';
+  public layoutType: string = 'dark'
+
+  constructor(public customize: CustomizerService, private router: Router) { 
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.url = event.url;
+      }
+    });
+  }
+
+  customizeLayoutVersion(val) {
+    this.customize.setLayoutVersion(val)
+    this.layoutType = val
+  }
+
+  customizeLayoutType(val) {
+    this.customize.setLayoutType(val)
+    this.layoutType = val
+  }
+
+
 }
