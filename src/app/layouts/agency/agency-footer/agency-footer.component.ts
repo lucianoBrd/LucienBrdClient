b49bbafb8570a3c73ea9f5546c11834dae754315
lucianoBrd <lucien.burdet@gmail.com>
2020-06-01 21:20:34 +0200

    footer

---
+++ src/app/layouts/agency/agency-footer/agency-footer.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Social } from '../models/social.interface';
+
+@Component({
+  selector: 'app-agency-footer',
+  templateUrl: './agency-footer.component.html',
+  styleUrls: ['./agency-footer.component.scss']
+})
+export class AgencyFooterComponent implements OnInit {
+
+  socials: Social[] = [
+    {
+      id: 1,
+      link: 'https://www.linkedin.com/in/lucien-burdet-b8b76a153',
+      fa: 'linkedin'
+    },
+    {
+      id: 2,
+      link: 'https://github.com/lucianoBrd',
+      fa: 'git'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
