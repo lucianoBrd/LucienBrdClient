1605f322c20ff18c902d13e7c355b4f821ab6c9c
lucianoBrd <lucien.burdet@gmail.com>
2020-06-01 21:06:20 +0200

    coyright

---
+++ src/app/layouts/agency/agency-copyright/agency-copyright.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agency-copyright',
+  templateUrl: './agency-copyright.component.html',
+  styleUrls: ['./agency-copyright.component.scss']
+})
+export class AgencyCopyrightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  year: number = new Date().getFullYear();
+
+}
