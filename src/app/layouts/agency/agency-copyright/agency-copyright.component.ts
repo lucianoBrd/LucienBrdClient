b42d66e6a887052079e49b135ca6495b4e2fd58e
lucien burdet <lucien.burdet@edf.fr>
2020-10-21 16:10:50 +0200

    policy

--- src/app/layouts/agency/agency-copyright/agency-copyright.component.ts
+++ src/app/layouts/agency/agency-copyright/agency-copyright.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Modal } from '../models/modal.interface';
 
 @Component({
   selector: 'app-agency-copyright',
@@ -6,12 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agency-copyright.component.scss']
 })
 export class AgencyCopyrightComponent implements OnInit {
+  public year: number = new Date().getFullYear();
+  public result: Modal;
 
-  constructor() { }
+  public modals: Modal[] = [
+    { 
+      id: 1,
+      title: "Politique de confidentialité",
+      description: "Je peux développer tous types de site : E-commerce, site vitrine, blog..."
+    },
+    { 
+      id: 2,
+      title: "Termes & Conditions",
+      description: "Vous ne savez pas comment bien utiliser les réseaux sociaux ? Je peux gérer vos comptes afin d’accroître votre notoriété."
+    },
+  ];
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
-  year: number = new Date().getFullYear();
+  
+
+  openVerticallyCentered(content, id: number) {
+    this.result = this.modals.find(x => x.id === id);
+
+    this.modalService.open(content, { centered: true, size: 'md' });
+  }
 
 }
