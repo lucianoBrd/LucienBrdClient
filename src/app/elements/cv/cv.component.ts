0d3efe6594579f2cde3d391d0f0969445777ac94
lucianoBrd <lucien.burdet@gmail.com>
2020-10-28 16:31:54 +0100

    get cv from api

--- src/app/elements/cv/cv.component.ts
+++ src/app/elements/cv/cv.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Cv } from 'src/app/shared/models/cv.interface';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.scss']
+  styleUrls: ['./cv.component.scss'],
+  providers: [DataService]
 })
-export class CvComponent implements OnInit {
+export class CvComponent implements OnInit, OnDestroy {
   public mobile: boolean = false;
-  
-  constructor() { }
+  public cv: Cv;
+  public documentPath: String;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private dataService: DataService) {
+    this.dataService.PAGE = '/cv';
+  }
 
   ngOnInit() {
     this.mobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data: any[]) => {
+      this.cv = data['cv'];
+      this.documentPath = data['documentPath'];
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
