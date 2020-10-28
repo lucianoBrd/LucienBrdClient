cad0e2215bd265f21e28f15b832c4dc563d60952
lucianoBrd <lucien.burdet@gmail.com>
2020-10-28 15:42:19 +0100

    get projects from api

--- src/app/elements/projects/projects.component.ts
+++ src/app/elements/projects/projects.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Project } from 'src/app/shared/models/project.interface';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  providers: [DataService]
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, OnDestroy {
 
-  projects: Project[] = [
-    
-  ];
+  public projects: Project[];
+  public imagePath: String;
+  public documentPath: String;
 
-  constructor() {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private dataService: DataService) {
+    this.dataService.PAGE = '/project';
   }
 
   ngOnInit() {
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data: any[]) => {
+      this.projects = data['projects'];
+      this.imagePath = data['imagePath'];
+      this.documentPath = data['documentPath'];
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
