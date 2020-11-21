4ca24cc305972e7d4817f2a1dcc27ef287a62125
lucianoBrd <lucien.burdet@gmail.com>
2020-11-21 15:02:22 +0100

    date interceptor for iso

--- src/app/shared/service/jsonDateInterceptor.service.ts
+++ src/app/shared/service/jsonDateInterceptor.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class JsonDateInterceptorService implements HttpInterceptor {
-  private _isoDateFormat = /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/g;
+  private _isoDateFormat = /^\d\d\d\d\/(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/g;
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(map((val: HttpEvent<any>) => {
